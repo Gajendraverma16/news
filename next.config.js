6dc9ee77b942fcf9803d6f4f08da59b6ceb6b345
Gajendra Verma <gajendraverma16@gmail.com>
2025-11-05 13:11:06 +0530

    Fix line endings configuration

--- next.config.js
+++ next.config.js
@@ -1,27 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
-require('dotenv').config()
+const path = require('path');
+require('dotenv').config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: [`${process.env.NEXT_PUBLIC_API_URL}`],
-    unoptimized: true // keep this if you want images unoptimized
+    unoptimized: true, // keep this if you want images unoptimized
   },
   trailingSlash: true,
   reactStrictMode: false,
   webpack: (config, { isServer }) => {
     if (isServer) {
-      require('./scripts/sitemap-generator')
+      require('./scripts/sitemap-generator');
     }
     config.resolve.alias = {
       ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
-    }
-    return config
-  }
-}
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision'),
+    };
+    return config;
+  },
+};
 
 // ⚠️ Do NOT set nextConfig.output = 'export' — keeps API routes working
-
-module.exports = nextConfig
+module.exports = nextConfig;
